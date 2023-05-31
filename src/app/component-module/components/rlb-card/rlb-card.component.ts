bea0e72f122daa6d7bec95bf65aab2fabdf35ef7
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2023-05-31 14:54:07 +0200

    edit card

--- src/app/component-module/components/rlb-card/rlb-card.component.ts
+++ src/app/component-module/components/rlb-card/rlb-card.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, ViewChild } from '@angular/core';
-import { ComponentData, ComponentItem } from '../component';
-import { AbstractComponent } from '../abstract-component';
-import { ComponentDirective } from '../component-module.directive';
+import { ComponentData, ComponentItem } from '../../component';
+import { AbstractComponent } from '../../abstract-component';
+import { ComponentDirective } from '../../component-module.directive';
+import { RlbCardData } from './rlb-card.data';
 
 @Component({
   template: `
@@ -15,8 +16,8 @@ import { ComponentDirective } from '../component-module.directive';
     </div>
   `
 })
-export class RlbCardComponent extends AbstractComponent implements ComponentData {
-  @Input() components: ComponentItem[] = [];
+export class RlbCardComponent extends AbstractComponent implements ComponentData<RlbCardData> {
+  @Input() components!: ComponentItem[];
   @ViewChild(ComponentDirective, { static: true }) component!: ComponentDirective;
-  @Input() data: any;
+  @Input() data!: RlbCardData;
 }
---
+++ src/app/component-module/components/rlb-card/rlb-card.data.ts
@@ -0,0 +1,6 @@
+export interface RlbCardData {
+  image?: string;
+  title?: string;
+  text?: string;
+  alt?: string;
+}
